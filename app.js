7e7699fd6c2ad25a34b92a56a778501af6bf3ac7
aiasifistiaque <43393686+aiasifistiaque@users.noreply.github.com>
2021-03-24 22:22:34 +0600

    Update app.js

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use('/api/question', questionRoute);
 app.use('/auth', authRouter);
 
 
-mongoose.connect('mongodb+srv://asifistiaque:root@cluster0-ctuoc.mongodb.net/v1?retryWrites=true&w=majority',{
+mongoose.connect(process.env.MONGO_URI,{
     useNewUrlParser:true, 
     useUnifiedTopology:true,
     useCreateIndex: true
